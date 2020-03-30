35430608d04cfdc9c168d063bdcaa40add56399f
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-03-30 18:59:36 +0300

    Created Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='n/a' src='https://robohash.org/random?200x200'></img>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
