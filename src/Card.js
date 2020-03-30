d1e0c48d5b02a1d2bb3f510049e4823b55401a09
Jolita Pundzaite <jolita.pundzaite@gmail.com>
2020-03-30 22:14:38 +0300

    Created CardList and robots components

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-        <div>
-            <img alt='n/a' src='https://robohash.org/random?200x200'></img>
+        <div className = 'tc bg-navy dib br4 pa3 ma3 grow bw2 shadow-5 light-gray'>
+            <img alt='n/a' src={`https://robohash.org/${id}?200x200`}></img>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
